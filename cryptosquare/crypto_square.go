eab7237ad637725ccc04832c99e18feec0f13e59
megafyk <megafyk@gmail.com>
2023-08-07 00:40:15 +0700

    init crypto square

---
+++ cryptosquare/crypto_square.go
@@ -0,0 +1,57 @@
+package cryptosquare
+
+import (
+	"math"
+	"regexp"
+	"strings"
+)
+
+func Encode(pt string) string {
+	var mx [][]string
+	var mxT [][]string
+
+	re := regexp.MustCompile("\\W")
+	pt = re.ReplaceAllString(pt, "")
+	pt = strings.ToLower(pt)
+	m := int(math.Sqrt(float64(len(pt))))
+	n := m
+	if m*m != len(pt) {
+		m++
+	}
+
+	var tmp []string
+	for i := 0; i < len(pt); i++ {
+		tmp = append(tmp, string(pt[i]))
+		if (i+1)%m == 0 {
+			mx = append(mx, tmp)
+			tmp = []string{}
+		}
+	}
+	mx = append(mx, tmp)
+
+	i := 0
+	for i < m {
+		var tmp []string
+		for j := 0; j < n; j++ {
+			if i < len(mx[j]) {
+				tmp = append(tmp, mx[j][i])
+			} else {
+				tmp = append(tmp, " ")
+			}
+			if j == n-1 {
+				mxT = append(mxT, tmp)
+				tmp = []string{}
+			}
+		}
+		i++
+	}
+
+	res := ""
+	for i := 0; i < len(mxT); i++ {
+		for j := 0; j < len(mxT[i]); j++ {
+			res += mxT[i][j]
+		}
+		res += " "
+	}
+	return res[:len(res)-1]
+}
---
+++ cryptosquare/crypto_square_test.go
@@ -0,0 +1,8 @@
+package cryptosquare
+
+import "testing"
+
+func TestEncode(t *testing.T) {
+	println(Encode("If man was meant to stay on the ground, god would have given us roots."))
+	println("imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ")
+}
