10a9ed2cee7371f46cb6c396166dacda2249b409
megafyk <megafyk@gmail.com>
2024-03-25 18:25:43 +0700

    add lc

--- lc/bit_manipulation/maximum-number-of-achievable-transfer-requests.py
+++ lc/bit_manipulation/maximum-number-of-achievable-transfer-requests.py
@@ -18,6 +18,16 @@ class Solution:
         res = 0
         # regconize a valid pattern (subset/mask) is a cycle. eg: [0,1],[1,0],[0,1],[1,2],[2,0] is 0 -> 1 -> 0 -> 1 -> 2 -> 0 (back to 0)
         # problem turn into find subset with max number of 1 bit
+
+        '''
+        The idea is to use a bitmask to represent a subset of transfer requests. 
+        Each bit in the bitmask corresponds to a transfer request in the original array. 
+        A 1 at a particular bit position indicates that the corresponding transfer request is included in the current subset, and a 0 indicates it's excluded.
+
+        We can iterate through all possible bitmasks (using a loop from 0 to 2^n - 1, where n is the number of requests) 
+        and check if the corresponding subset of requests is achievable.
+        A subset is achievable if the net change in the number of employees for each building is zero.
+        '''
         for mask in range(1<<m):
             if self.check_subset(n, requests, mask):
                 res = max(res, mask.bit_count())
