3578adfe4481019fbabf92340eba061a36bd39b1
megafyk <megafyk@gmail.com>
2024-03-26 17:36:20 +0700

    add lc

---
+++ lc/bit_manipulation/triples-with-bitwise-and-equal-to-zero.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def countTriplets(self, nums: List[int]) -> int:
+#         # complexity O(n^3)
+#         # n = len(nums)
+#         # cnt = 0
+#         # for i in range(n):
+#         #     for j in range(n):
+#         #         for k in range(n):
+#         #             if (nums[i] & nums[j] & nums[k]) == 0:
+#         #                 cnt += 1
+#         # return cnt
+
+        n = len(nums)
+        cnt = 0
+        freq = {}
+        for x in nums:
+            for y in nums:
+                if x & y not in freq:
+                    freq[x&y] = 0
+                freq[x&y] += 1
+        
+        for i in nums:
+            for j in freq:
+                if i & j == 0:
+                    cnt += freq[j]
+        return cnt
