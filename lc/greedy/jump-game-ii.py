8838b28818fba7c4e67de72e1afcc703b4adfa52
megafyk <megafyk@gmail.com>
2024-03-28 15:35:14 +0700

    add lc

--- lc/greedy/jump-game-ii.py
+++ lc/greedy/jump-game-ii.py
@@ -15,4 +15,27 @@ class Solution:
         #             dp[nxt_idx] = min(dp[nxt_idx], dp[i] + 1)
 
         # return dp[n-1] if dp[n-1] != n else -1
-        return -1
+        
+
+        n = len(nums)
+        idx_cur = 0
+        idx_nxt = 0
+        cnt = 0
+
+        while idx_cur < n - 1:
+            idx_nxt = idx_cur
+            for j in range(idx_cur, idx_cur + nums[idx_cur] + 1):
+                if j < n-1 and j + nums[j] >= idx_nxt + nums[idx_nxt]:
+                    # update new target idx_nxt
+                    idx_nxt = j
+
+            print(idx_cur, idx_nxt, idx_cur + nums[idx_cur], n - 1)
+            if idx_cur + nums[idx_cur] >= n - 1:
+                idx_cur = n
+            else:
+                idx_cur = idx_nxt
+            print(idx_cur, idx_nxt)
+            cnt += 1
+            
+
+        return cnt
