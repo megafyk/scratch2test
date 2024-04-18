a2020159a9a0fbfbcdd6ce6403bf48bd336cb34e
megafyk <megafyk@gmail.com>
2024-04-18 13:08:49 +0700

    add lc

---
+++ lc/two_pointer/boats-to-save-people.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+
+        # complexity: time O(n), mem O(1)
+        people = sorted(people)
+        n = len(people)
+        l,r = 0,n-1
+        boat = 0
+
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+                r -= 1
+            else:
+                r -= 1
+            boat += 1
+        return boat
