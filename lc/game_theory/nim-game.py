6b7acb38b50e10548af2e8bbed79088e64e1bb67
megafyk <megafyk@gmail.com>
2024-05-13 21:21:22 +0700

    add lc

---
+++ lc/game_theory/nim-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n & 3
+        # dp = [True] * (n+1)
+        # for i in range(4,n+1):
+        #     if dp[i-3] and dp[i-2] and dp[i-1]:
+        #         dp[i] = False
+        # return dp[-1]
