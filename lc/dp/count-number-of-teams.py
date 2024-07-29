30b4149130e3989146946d3ff54dd62e6281ca55
megafyk <megafyk@gmail.com>
2024-07-29 16:06:55 +0700

    add lc
    add lc

--- lc/dp/count-number-of-teams.py
+++ lc/dp/count-number-of-teams.py
@@ -1,26 +1,60 @@
 class Solution:
-    def dp(self, rating, memo, incr, idx, matched):
-        if matched == 3:
-            return 1
-        if (idx, matched, incr) in memo:
-            return memo[(idx, matched, incr)]
-        ans = 0
-        for i in range(idx + 1, len(rating)):
-            if (incr and rating[idx] < rating[i]) or (
-                not incr and rating[idx] > rating[i]
-            ):
-                ans += self.dp(rating, memo, incr, i, matched + 1)
-        memo[(idx, matched, incr)] = ans
-        return ans
 
     def numTeams(self, rating: List[int]) -> int:
-        memo = {}
+        # dp
+        # complexity: time O(n^2), mem O(1)
+        n = len(rating)
         ans = 0
-        for i in range(len(rating)):
-            incr = self.dp(rating, memo, True, i, 1)
-            desc = self.dp(rating, memo, False, i, 1)
-            ans += incr + desc
+        for i in range(n):
+            left_incr = 0
+            right_incr = 0
+
+            for j in range(0, i):
+                if rating[j] < rating[i]:
+                    left_incr += 1
+
+            for j in range(i + 1, n):
+                if rating[i] < rating[j]:
+                    right_incr += 1
+            ans += left_incr * right_incr
+
+            left_desc = 0
+            right_desc = 0
+
+            for j in range(0, i):
+                if rating[j] > rating[i]:
+                    left_desc += 1
+
+            for j in range(i + 1, n):
+                if rating[i] > rating[j]:
+                    right_desc += 1
+
+            ans += left_desc * right_desc
+
         return ans
+
+    # def dp(self, rating, memo, incr, idx, matched):
+    #     if matched == 3:
+    #         return 1
+    #     if (idx, matched, incr) in memo:
+    #         return memo[(idx, matched, incr)]
+    #     ans = 0
+    #     for i in range(idx + 1, len(rating)):
+    #         if (incr and rating[idx] < rating[i]) or (
+    #             not incr and rating[idx] > rating[i]
+    #         ):
+    #             ans += self.dp(rating, memo, incr, i, matched + 1)
+    #     memo[(idx, matched, incr)] = ans
+    #     return ans
+    #
+    # def numTeams(self, rating: List[int]) -> int:
+    #     memo = {}
+    #     ans = 0
+    #     for i in range(len(rating)):
+    #         incr = self.dp(rating, memo, True, i, 1)
+    #         desc = self.dp(rating, memo, False, i, 1)
+    #         ans += incr + desc
+    #     return ans
 
     # def numTeams(self, rating: List[int]) -> int:
     #     # brute force
