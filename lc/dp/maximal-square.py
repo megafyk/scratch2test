c4309ebbde7dfe4ce3bef7b846940073295a91f7
megafyk <megafyk@gmail.com>
2024-08-01 22:15:33 +0700

    add lc
    add lc

---
+++ lc/dp/maximal-square.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # dp with matrix
+        # complexity: time O(m*n), mem O(1)
+        m = len(matrix)
+        n = len(matrix[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                matrix[i][j] = int(matrix[i][j])
+                if matrix[i][j] and i >= 1 and j >= 1:
+                    matrix[i][j] = (
+                        min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1])
+                        + 1
+                    )
+                    ans = max(ans, matrix[i][j])
+                elif matrix[i][j]:
+                    matrix[i][j] == 1
+                    ans = max(ans, matrix[i][j])
+        return ans * ans
