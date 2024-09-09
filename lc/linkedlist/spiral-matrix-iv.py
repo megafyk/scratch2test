f2bd9e64ad829b5c45cfe5c36e7d0ccdd106ff13
megafyk <megafyk@gmail.com>
2024-09-09 15:09:05 +0700

    update lc
    update lc

--- lc/linkedlist/spiral-matrix-iv.py
+++ lc/linkedlist/spiral-matrix-iv.py
@@ -9,24 +9,16 @@ class Solution:
         # complexity: time O(m*n), mem O(m*n)
         res = [[-1] * n for _ in range(m)]
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        dir_idx = 0
-        x, y = 0, 0
+        idx = 0
+        x, y = 0,-1
         cur_node = head
         while cur_node:
-            res[x][y] = cur_node.val
-            dx, dy = dirs[dir_idx % 4]
+            dx, dy = dirs[idx]
             tmp_x, tmp_y = x + dx, y + dy
-            if (
-                tmp_x < 0
-                or tmp_x >= m
-                or tmp_y < 0
-                or tmp_y >= n
-                or res[tmp_x][tmp_y] != -1
-            ):
-                dir_idx += 1
-                dx, dy = dirs[dir_idx % 4]
-                tmp_x, tmp_y = x + dx, y + dy
+            if tmp_x < 0 or tmp_x >= m or tmp_y < 0 or tmp_y >= n or res[tmp_x][tmp_y] != -1:
+                idx = (idx + 1) % 4
+                continue
             x, y = tmp_x, tmp_y
+            res[x][y] = cur_node.val
             cur_node = cur_node.next
-
         return res
