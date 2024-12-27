34639c287666f7337224864dd66b28ca93fd0043
megafyk <megafyk@gmail.com>
2024-12-27 08:55:40 +0700

    add lc

--- lc/dp/best-sightseeing-pair.py
+++ lc/dp/best-sightseeing-pair.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        # dp -> (values[i]+i) + (values[j]-j) (i<j)
+        # time O(n), space O(1)
         n = len(values)
-        mx, dp = values[0], values[0]
+        dp, mx = values[0], values[0]
 
         for i in range(1, n):
-            dp = max(dp, values[i - 1] + i - 1)
+            dp = max(dp, values[i-1] + i-1)
             mx = max(mx, dp + values[i] - i)
         return mx
