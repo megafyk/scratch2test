e07f985ea87832c0d08a5385a011c67f7a0cbb8c
megafyk <megafyk@gmail.com>
2025-01-03 22:32:06 +0700

    add lc

---
+++ lc/dp/string/unique-substrings-in-wraparound-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findSubstringInWraproundString(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * 26
+        curlen = 1
+        for i in range(n):
+            if i > 0 and ord(s[i]) - ord(s[i-1]) == 1 or ord(s[i-1]) - ord(s[i]) == 25:
+                curlen += 1
+            else:
+                curlen = 1
+            idx = ord(s[i]) - ord('a')
+            dp[idx] = max(dp[idx], curlen)
+        return sum(dp)
