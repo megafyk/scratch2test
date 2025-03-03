7c085abc69e880259af9eeae100a1717be3ca540
megafyk <megafyk@gmail.com>
2025-03-03 17:13:40 +0700

    add lc

---
+++ lc/array/partition-array-according-to-given-pivot.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # array
+        # time O(N), space O(N)
+        smaller = []
+        equal = []
+        bigger = []
+        for num in nums:
+            if num < pivot:
+                smaller.append(num)
+            elif num > pivot:
+                bigger.append(num)
+            else:
+                equal.append(num)
+        return smaller + equal + bigger
+        
