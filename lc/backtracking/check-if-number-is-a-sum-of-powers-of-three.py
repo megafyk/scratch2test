c99389279427a9a4146d3a5e8a93f0e623625111
megafyk <megafyk@gmail.com>
2025-03-04 18:33:26 +0700

    add lc

--- lc/backtracking/check-if-number-is-a-sum-of-powers-of-three.py
+++ lc/backtracking/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,5 +1,22 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
+        # greedy
+        # time O(logn), space O(1)
+        i = 0
+        while 3 ** i <= n:
+            i += 1
+
+        while i >= 0:
+            power = 3 ** i
+            if power <= n:
+                n -= power
+            if power <= n:
+                return False
+            i -= 1
+        return n == 0
+
+
+    def checkPowersOfThree(self, n: int) -> bool:
         # backtrack
         # time O(3^k), space O(1)
         def backtrack(x, cur_sum):
