d12b57ad0d8c1e68c42ccfadf5ce0132507c8012
megafyk <megafyk@gmail.com>
2025-04-06 22:58:50 +0700

    add lc

---
+++ lc/tree/lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root):
+        # dfs
+        # time O(N), space O(N)
+        def helper(root):
+            if not root: return 0, None
+            h1, lca1 = helper(root.left)
+            h2, lca2 = helper(root.right)
+            if h1 > h2: return h1 + 1, lca1
+            if h1 < h2: return h2 + 1, lca2
+            return h1 + 1, root
+        return helper(root)[1]
+        
