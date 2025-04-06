7ba958ccef4eb4f4786d17fc2793720cfa705ece
megafyk <megafyk@gmail.com>
2025-04-06 09:10:33 +0700

    add lc

--- lc/dp/distinct_ways/largest-divisible-subset.py
+++ lc/dp/distinct_ways/largest-divisible-subset.py
@@ -1,15 +1,14 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         # dp distinct ways
-        # time O(n^2), space O(n)
+        # time O(NlogN + N^2), space O(N + N^2)
+
         n = len(nums)
         nums = sorted(nums)
         dp = [1] * n
-        path = [[nums[i]] for i in range(n)]
-
-        res = [nums[0]]
-
-        mx = 1
+        path = [[num] for num in nums]
+        res = path[0]
+        mx_len = 1
         for i in range(1, n):
             for j in range(i):
                 if nums[i] % nums[j] == 0:
@@ -17,7 +16,7 @@ class Solution:
                         dp[i] = dp[j] + 1
                         path[i] = list(path[j])
                         path[i].append(nums[i])
-                        if len(path[i]) > mx:
-                            mx = len(path[i])
+                        if len(path[i]) > mx_len:
+                            mx_len = len(path[i])
                             res = path[i]
         return res
