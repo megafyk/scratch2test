57c2b39e0de9d798780f99ccf8d634f2b41c9e76
megafyk <megafyk@gmail.com>
2025-04-18 22:28:40 +0700

    add lc

--- lc/sliding_window/minimum-size-subarray-sum.py
+++ lc/sliding_window/minimum-size-subarray-sum.py
@@ -1,24 +1,16 @@
-from typing import List
-
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
-        slow, fast = 0, 0
-        win_sum = 0
-        ans = len(nums) + 1
-        while fast < len(nums):
-            win_sum += nums[fast]
-            while win_sum >= target:
-                if fast - slow + 1 < ans:
-                    ans = fast - slow + 1
-                win_sum -= nums[slow]
-                slow += 1
-            fast += 1
-        if ans == len(nums) + 1:
-            return 0
-        return ans
-
-
-s = Solution()
-print(s.minSubArrayLen(7, [2,3,1,2,4,3]))
-print(s.minSubArrayLen(11, [1,1,1,1,1,1,1,1,1]))
+        # sliding window
+        # time O(N), space O(1)
+        if sum(nums) < target: return 0
+        i = 0
+        N = len(nums)
+        cur_sum = 0
+        res = N
+        for j in range(N):
+            cur_sum += nums[j]
+            while cur_sum >= target:
+                res = min(res, j-i+1)
+                cur_sum -= nums[i]
+                i += 1
+        return res
