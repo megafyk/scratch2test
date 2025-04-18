681dc90acd8e420c8d7e07131d6fd6e09bab575a
megafyk <megafyk@gmail.com>
2025-04-18 21:24:14 +0700

    add lc

--- lc/sliding_window/longest-repeating-character-replacement.py
+++ lc/sliding_window/longest-repeating-character-replacement.py
@@ -1,21 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        slow = fast = 0
-        ans = 0
-        counter = [0 for _ in range(26)]
-        max_count = 0
-        while fast < len(s):
-            idx = ord(s[fast]) - ord('A')
-            counter[idx] += 1
-            max_count = max(max_count, counter[idx])
-            while fast - slow + 1 - max_count > k:
-                counter[ord(s[slow]) - ord('A')] -= 1
-                slow += 1
-            ans = max(ans, fast - slow + 1)
-            fast += 1
-        return ans
+        # sliding window
+        # time O(N), space O(1)
+        i = j = 0
+        counter = [0] * 26
+        N = len(s)
+        mx_counter = 0
+        res = 0
+        while j < N:
+            ch_idx = ord(s[j]) - ord('A')
+            counter[ch_idx] += 1
+            mx_counter = max(mx_counter, counter[ch_idx])
+            while (j-i+1) - mx_counter > k:
+                counter[ord(s[i]) - ord('A')] -= 1
+                i += 1
+            res = max(res, j-i+1)
+            j += 1
 
-s = Solution()
-print(s.characterReplacement("AABABBA", 1))
-print(s.characterReplacement("ABAB", 2))
-print(s.characterReplacement("ABAA", 0))
+        return res
