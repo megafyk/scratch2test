21d37bcce97455f793f7ed776517a91b53e634eb
megafyk <megafyk@gmail.com>
2025-04-19 12:24:55 +0700

    add lc

--- lc/binary_search/count-the-number-of-fair-pairs.py
+++ lc/binary_search/count-the-number-of-fair-pairs.py
@@ -1,21 +1,21 @@
 class Solution:
-    def bin_search(self, nums, target, l, r):
-        while l < r:
-            mid = l + (r-l) // 2
-            if nums[mid] > target:
-                r = mid
-            else:
-                l = mid + 1
-        return l
-
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         # binary search
-        # time O(nlogn), space O(n)
-        n = len(nums)
+        # time O(NlogN), space O(1)
+        def search(target, l, r):
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] > target:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
+
+        N = len(nums)
         nums = sorted(nums)
         res = 0
-        for i in range(n):
+        for i in range(N):
             low = lower - nums[i]
             up = upper - nums[i]
-            res += (self.bin_search(nums, up, i+1, n) - self.bin_search(nums, low-1, i+1, n))
+            res += search(up, i, N) - search(low-1, i, N)
         return res
