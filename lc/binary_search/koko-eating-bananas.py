fec5cf0ff92e6027e84ab53eda9ae4cc3e21f847
megafyk <megafyk@gmail.com>
2025-05-03 09:49:13 +0700

    add lc

--- lc/binary_search/koko-eating-bananas.py
+++ lc/binary_search/koko-eating-bananas.py
@@ -1,26 +1,40 @@
-from typing import List
-
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def eat(can: int):
-            count = 0
-
-            for p in piles:
-                count += (p-1) // can + 1
-
-            return count <= h
-
+        N = len(piles)
         left, right = 1, max(piles)
 
         while left < right:
             mid = left + (right - left) // 2
-            if eat(mid):
+            if sum([math.ceil(piles[i] / mid) for i in range(N)]) <= h:
                 right = mid
             else:
                 left = mid + 1
-        return left 
 
-s = Solution()
-print(s.minEatingSpeed([3,6,7,11], 8))
-print(s.minEatingSpeed([312884470], 312884469))
+        return left
+
+# from typing import List
+
+
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+#         def eat(can: int):
+#             count = 0
+
+#             for p in piles:
+#                 count += (p-1) // can + 1
+
+#             return count <= h
+
+#         left, right = 1, max(piles)
+
+#         while left < right:
+#             mid = left + (right - left) // 2
+#             if eat(mid):
+#                 right = mid
+#             else:
+#                 left = mid + 1
+#         return left 
+
+# s = Solution()
+# print(s.minEatingSpeed([3,6,7,11], 8))
+# print(s.minEatingSpeed([312884470], 312884469))
