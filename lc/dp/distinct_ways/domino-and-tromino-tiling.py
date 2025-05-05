e769bb70ec6cf5d61561d846275b21d058172a67
megafyk <megafyk@gmail.com>
2025-05-05 09:30:43 +0700

    add lc

--- lc/dp/distinct_ways/domino-and-tromino-tiling.py
+++ lc/dp/distinct_ways/domino-and-tromino-tiling.py
@@ -1,17 +1,18 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        # dp distinct ways
-        # complexity: time O(n), mem O(1)
+        # dp count distinct ways
+        # dp[i] (full) = dp[i-1] (full) + dp[i-2] (full) + dp_top[i-1] (miss top) + dp_bottom[i-1] (miss bot)
+        # time O(N), space O(1)
         mod = 10**9 + 7
-        dp2,dp1,dp_b,dp_t = 1, 1, 0, 0
+        dp2, dp1, dp_top, dp_bottom = 1, 1, 0, 0
         for i in range(2, n + 1):
-            dp2, dp1, dp_b, dp_t = (
-                (dp2 + dp1 + dp_b + dp_t) % mod,
+            dp2, dp1, dp_top, dp_bottom = (
+                dp2 + dp1 + dp_top + dp_bottom,
                 dp2,
-                dp_b + dp1,
-                dp_t + dp1,
+                dp_top + dp1,
+                dp_bottom + dp1,
             )
-        return dp2
+        return dp2 % mod
 
         # return dp[-1]
         # dp,dp_b,dp_t = [0]*(n+1),[0]*(n+1),[0]*(n+1)
