9c039f68563ca34dc19e18a2d82cb3b5c6244598
megafyk <megafyk@gmail.com>
2025-05-06 09:36:06 +0700

    add lc

--- lc/array/build-array-from-permutation.py
+++ lc/array/build-array-from-permutation.py
@@ -1,3 +1,14 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[nums[i]] for i in range(len(nums))]
+        # array
+        # time O(N), space O(1)
+        N = len(nums)
+        for i in range(N):
+            nums[i] = nums[i] + N * (nums[nums[i]] % N) # euclid encode a = bq + r (a = nums[nums[i]], b = nums[i])
+        for i in range(N):
+            nums[i] = nums[i] // N
+        return nums
+    
+# class Solution:
+#     def buildArray(self, nums: List[int]) -> List[int]:
+#         return [nums[nums[i]] for i in range(len(nums))]
