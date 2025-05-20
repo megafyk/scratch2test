02c80e7dcf06aa32f4d1059a781d6ef9aaf5a5ee
megafyk <megafyk@gmail.com>
2025-05-20 13:53:45 +0700

    add lc

--- lc/array/zero-array-transformation-i.py
+++ lc/array/zero-array-transformation-i.py
@@ -3,15 +3,15 @@ class Solution:
         # prefix sum
         # time O(n+q), space O(n)
         n = len(nums)
-        delta_log = [0] * n # log tracking
+        delta_log = [0] * (n+1) # log tracking
         for start,end in queries:
             delta_log[start] += 1
-            if end < n-1: delta_log[end+1] -= 1
-        prefix = [0] * n
-        prefix[0] = delta_log[0]
-        for i in range(1, n):
-            prefix[i] = prefix[i-1] + delta_log[i]
+            delta_log[end+1] -= 1
+        
+        cur = 0
+        
         for i in range(n):
-            if nums[i] > prefix[i]:
+            cur += delta_log[i]
+            if nums[i] > cur:
                 return False
         return True
