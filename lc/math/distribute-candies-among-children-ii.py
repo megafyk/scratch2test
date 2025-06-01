c0476446f6563d644b878f2bad99158c52b2d5cb
megafyk <megafyk@gmail.com>
2025-06-01 09:09:29 +0700

    add lc

--- lc/math/distribute-candies-among-children-ii.py
+++ lc/math/distribute-candies-among-children-ii.py
@@ -3,6 +3,13 @@ class Solution:
         # combinatoric contraction
         # time O(1), space O(1)
         def calc(x):
-            if x <= 0: return 0
-            return x*(x-1) // 2
-        return calc(n+2) - 3*calc(n-limit+1) + 3 * calc(n-2*(limit+1) + 2) - calc(n-3*(limit+1) + 2)
+            if x <= 0:
+                return 0
+            return x * (x - 1) // 2
+
+        return (
+            calc(n + 2)
+            - 3 * calc(n - (limit + 1) + 2)
+            + 3 * calc(n - 2 * (limit + 1) + 2)
+            - calc(n - 3 * (limit + 1) + 2)
+        )
