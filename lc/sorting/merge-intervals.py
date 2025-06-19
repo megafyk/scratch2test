73a7df732dcfd3c4f25142255520d92d9b156f62
megafyk <megafyk@gmail.com>
2025-06-19 22:56:48 +0700

    add lc

--- lc/sorting/merge-intervals.py
+++ lc/sorting/merge-intervals.py
@@ -1,21 +1,20 @@
-from functools import cmp_to_key
 class Solution:
-    def cmp(self, a, b):
-        if a[0] == b[0]: return 1 if a[1] > b[1] else -1
-        else: return 1 if a[0] > b[0] else -1
-
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # array sorting
+        # sorting + greedy
         # time O(nlogn), space O(n)
-        intervals = sorted(intervals, key=cmp_to_key(self.cmp))
+        intervals = sorted(intervals, key=lambda x: x[0])
+
         n = len(intervals)
-        res = []
         cur = intervals[0]
+        res = []
         for i in range(1, n):
-            if intervals[i][0] > cur[1]:
+            s = intervals[i][0]
+            e = intervals[i][1]
+            if cur[1] >= s: # do merge
+                cur[1] = e if cur[1] < e else cur[1]
+            else:
                 res.append(cur)
                 cur = intervals[i]
-            else:
-                cur[1] = intervals[i][1] if intervals[i][1] > cur[1] else cur[1]
         res.append(cur)
         return res
+
