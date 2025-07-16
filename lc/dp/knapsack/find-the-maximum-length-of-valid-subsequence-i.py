155d2a8fcc2cf596c6afc062734d13e40bdc96cb
megafyk <megafyk@gmail.com>
2025-07-16 08:10:49 +0700

    add lc

---
+++ lc/dp/knapsack/find-the-maximum-length-of-valid-subsequence-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        # dp knapsack 0/1
+        # time O(n), space O(1)
+        t = nums[0] % 2
+        all_odd = all_even = both = 0
+        for num in nums:
+            if num % 2 == 0:
+                all_even +=1
+            else:
+                all_odd += 1
+            
+            if num % 2 == t:
+                both += 1
+                t ^= 1 # toggle
+        return max(both, all_odd, all_even)
