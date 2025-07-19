b34c15cf281f4dd087540c9b02e5451016ce7a8e
megafyk <megafyk@gmail.com>
2025-07-19 10:04:15 +0700

    add lc

--- lc/trie/remove-sub-folders-from-the-filesystem.py
+++ lc/trie/remove-sub-folders-from-the-filesystem.py
@@ -1,8 +1,46 @@
 class TrieNode:
     def __init__(self):
+        self.child = {}
+        self.isfolder = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # trie
+        # time O(nlogn + )
+        folder.sort(key = lambda x: len(x))
+        
+        trie = Trie()
+        for f in folder:
+            node = trie.root
+            words = f.split("/")[1:]
+            for i, w in enumerate(words):
+                if node.isfolder: # optimize
+                    break
+                if w not in node.child:
+                    node.child[w] = TrieNode() 
+                node = node.child[w]
+                if i == len(words) - 1:
+                    node.isfolder = True
+        
+        res = []
+        def search(node, prefix):
+            if node.isfolder:
+                res.append(prefix)
+                return
+            for k,v in node.child.items():
+                search(v, prefix + "/" + k)
+        search(trie.root, "")
+        return res
+
+
+class TrieNode1:
+    def __init__(self):
         self.exists = False
         self.child = {}
-class Trie:
+class Trie1:
     def __init__(self, arr):
         self.root = TrieNode()
         self.path = set()
@@ -18,7 +56,7 @@ class Trie:
                     break
             cur.exists = True
             if add: self.path.add(f)
-class Solution:
+class Solution1:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         # time O(n*m + nlogn), space O(n*m)
         trie = Trie(sorted(folder))
