25dd4e2f245362d7921ddea65232e33988edc423
megafyk <megafyk@gmail.com>
2025-07-28 13:56:02 +0700

    add lc

--- lc/backtracking/count-number-of-maximum-bitwise-or-subsets.py
+++ lc/backtracking/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,4 +1,33 @@
 class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        n = len(nums)
+        best = 0
+        for num in nums:
+            best |= num
+            
+        @cache
+        def dfs(cur, idx):
+            if idx >= n:
+                return 1 if cur == best else 0
+            return dfs(cur | nums[idx], idx + 1) + dfs(cur, idx + 1)
+        return dfs(0, 0)
+
+class Solution1:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        cnt = defaultdict(int)
+        n = len(nums)
+
+        def backtrack(idx, cur_or):
+            if idx >= n:
+                return
+            cnt[cur_or | nums[idx]] += 1
+            backtrack(idx + 1, cur_or | nums[idx])
+            backtrack(idx + 1, cur_or)
+
+        backtrack(0, 0)
+        return max(cnt.values())
+
+class Solution2:
     def backtrack(self, nums, target, i, cur_or, cur):
         count = 0
         if cur_or == target:
