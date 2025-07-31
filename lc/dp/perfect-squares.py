cac876e6ba6250142e582fe0e3d0092f11fd9029
megafyk <megafyk@gmail.com>
2025-07-31 22:26:15 +0700

    add lc

--- lc/dp/perfect-squares.py
+++ lc/dp/perfect-squares.py
@@ -1,15 +1,33 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        # dp
-        # complexity: time O(n), mem O(n)
-        dp = [float("inf") for _ in range(n + 1)]
+        # dp bottom up knapsack 0/1
+        # time O(nlogn), space O(n)
+        inf = 10**9 + 7
+        dp = [inf] * (n + 1)
         dp[0] = 0
-
+        dp[1] = 1
         for i in range(1, n + 1):
             j = 1
-            mi = float("inf")
             while j * j <= i:
-                mi = min(mi, dp[i - j * j] + 1)
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
                 j += 1
-            dp[i] = mi
-        return dp[-1]
+        return dp[n]
+
+
+class Solution1:
+    def numSquares(self, n: int) -> int:
+        # dp topdown
+        @cache
+        def dfs(i):
+            if i == 0:
+                return 0
+            if i == 1:
+                return 1
+            j = 1
+            res = sys.maxsize
+            while j * j <= i:
+                res = min(res, dfs(i - j * j) + 1)
+                j += 1
+            return res
+
+        return dfs(n)
