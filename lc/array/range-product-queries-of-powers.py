3a239652973a9940405de77c2fbce244a9cd4412
megafyk <megafyk@gmail.com>
2025-08-11 09:42:26 +0700

    add lc

--- lc/array/range-product-queries-of-powers.py
+++ lc/array/range-product-queries-of-powers.py
@@ -1,20 +1,19 @@
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        # array
+        # prefix sum
         # time O(q), space O(1)
-        mod = 10 ** 9 + 7
-        powers = []
+        mod = 10**9 + 7
+        prefix = []
         for i in range(32):
             if (n >> i) & 1 == 1:
-                powers.append(2**i)
-        prefix = [0] * len(powers)
-        prefix[0] = powers[0]
-        for i in range(1, len(powers)):
-            prefix[i] = (prefix[i-1] * powers[i])
+                if len(prefix) == 0:
+                    prefix.append(2**i)
+                else:
+                    prefix.append(prefix[-1] * 2**i)
 
         res = []
-        for s,e in queries:
+        for s, e in queries:
             t = prefix[e]
-            t //= prefix[s-1] if s > 0 else 1
+            t //= prefix[s - 1] if s > 0 else 1
             res.append(t % mod)
         return res
