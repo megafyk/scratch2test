a6de0f0e5469bbaa8f43e65b5734aa72354c756b
megafyk <megafyk@gmail.com>
2025-09-24 22:35:28 +0700

    add lc

--- lc/dp/distinct_ways/number-of-dice-rolls-with-target-sum.py
+++ lc/dp/distinct_ways/number-of-dice-rolls-with-target-sum.py
@@ -1,4 +1,22 @@
 class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        if target < n or target > n * k:
+            return 0
+        mod = 10**9 + 7
+        dp = [0] * (max(target,k)+1)
+        for i in range(1, k+1):
+            dp[i] = 1
+
+        for i in range(2, n+1):
+            dp2 = [0] * (max(target,k)+1)
+            for t in range(1, target+1):
+                for j in range(1, k+1):
+                    if t - j >= 1:
+                        dp2[t] = (dp2[t] + dp[t-j]) % mod
+            dp = dp2
+        return dp[target]
+
+class Solution1 :
     def dp(self, n, k, target, memo):
         mod = 10**9 + 7
         if n == 0 and target == 0:
