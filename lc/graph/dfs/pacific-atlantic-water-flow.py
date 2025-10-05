f82380a89461a260aeacd002a3c9d30ab418849c
megafyk <megafyk@gmail.com>
2025-10-05 08:11:54 +0700

    add lc

--- lc/graph/dfs/pacific-atlantic-water-flow.py
+++ lc/graph/dfs/pacific-atlantic-water-flow.py
@@ -1,7 +1,52 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # dfs graph matrix
+        # bfs
         # time O(m*n), space O(m*n)
+        m = len(heights)
+        n = len(heights[0])
+        
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        def bfs(q, visit):
+            while q:
+                x,y = q.popleft()
+                for dx,dy in dirs:
+                    nx,ny = x+dx,y+dy
+                    if 0<=nx<m and 0<=ny<n and not visit[nx][ny] and heights[nx][ny] >= heights[x][y]:
+                        visit[nx][ny] = True
+                        q.append((nx,ny))
+        
+        pac = [[False] * n for _ in range(m)]
+        q_pac = deque()
+        for j in range(n):
+            q_pac.append((0, j))
+            pac[0][j] = True
+        for i in range(m):
+            q_pac.append((i,0))
+            pac[i][0] = True
+
+        bfs(q_pac, pac)
+        
+        alt = [[False] * n for _ in range(m)]
+        q_alt = deque()
+        for j in range(n):
+            q_alt.append((m-1, j))
+            alt[m-1][j] = True
+        for i in range(m):
+            q_alt.append((i,n-1))
+            alt[i][n-1] = True
+
+        bfs(q_alt, alt)
+
+        res = []
+        for i in range(m):
+            for j in range(n):
+                if pac[i][j] and alt[i][j]:
+                    res.append([i,j])
+        return res
+
+class Solution1:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # dfs
         m = len(heights)
         n = len(heights[0])
 
