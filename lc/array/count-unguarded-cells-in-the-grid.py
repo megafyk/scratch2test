f69ea858bfc29a42ac82bd44e308e0366433b629
megafyk <megafyk@gmail.com>
2025-11-04 21:50:01 +0700

    add lc

--- lc/array/count-unguarded-cells-in-the-grid.py
+++ lc/array/count-unguarded-cells-in-the-grid.py
@@ -1,4 +1,42 @@
 class Solution:
+    def countUnguarded(
+        self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]
+    ) -> int:
+        q = deque()
+        visit = set()
+        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        walls = set([(x, y) for x, y in walls])
+        guards = set([(x, y) for x, y in guards])
+        for x, y in guards:
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if (
+                    0 <= nx < m
+                    and 0 <= ny < n
+                    and (nx, ny) not in walls
+                    and (nx, ny) not in guards
+                    and (nx, ny, dx, dy) not in visit
+                ):
+                    q.append((nx, ny, dx, dy))
+                    visit.add((nx, ny, dx, dy))
+
+        while q:
+            x, y, dx, dy = q.popleft()
+            nx, ny = x + dx, y + dy
+            if (
+                0 <= nx < m
+                and 0 <= ny < n
+                and ((nx, ny) not in walls)
+                and ((nx, ny) not in guards)
+                and ((nx, ny, dx, dy) not in visit)
+            ):
+                q.append((nx, ny, dx, dy))
+                visit.add((nx, ny, dx, dy))
+
+        visit = set([(x, y) for x, y, _, _ in visit])
+        return m * n - len(visit) - len(guards) - len(walls)
+
+class Solution1:
 
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         # array
