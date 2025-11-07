8d12a68c93a11598f62e4599a1c2104c279ca08c
megafyk <megafyk@gmail.com>
2025-11-07 13:58:15 +0700

    add lc

--- lc/binary_search/template.py
+++ lc/binary_search/template.py
@@ -4,7 +4,7 @@ def binary_search_min_k(array) -> int:
 
     left, right = min(search_space), max(search_space) # could be [0, n], [1, n] etc. Depends on problem
     while left < right:
-        mid = left + (right - left) // 2
+        mid = (left + right) // 2
         if condition(mid):
             right = mid # move left to find smaller k
         else:
@@ -18,7 +18,7 @@ def binary_search_max_k(array) -> int:
 
     left, right = min(search_space), max(search_space) # could be [0, n], [1, n] etc. Depends on problem
     while left < right:
-        mid = left + (right - left) // 2
+        mid = (left + right + 1) // 2
         if condition(mid):
             left = mid # move right to find larger k
         else:
