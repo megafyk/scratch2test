9da35db7fb56c89aeb4fd4b505a07a4a43aa6154
megafyk <megafyk@gmail.com>
2025-12-28 23:24:03 +0700

    add lc contest 482

--- lc/binary_search/minimum-cost-to-acquire-required-items.py
+++ lc/binary_search/minimum-cost-to-acquire-required-items.py
@@ -1,5 +1,14 @@
 class Solution:
     def minimumCost(self, c1: int, c2: int, c3: int, n1: int, n2: int) -> int:
+        min_cost = min(c1 + c2, c3) * min(n1, n2)
+        if n1 > n2:
+            min_cost += min(c1, c3) * (n1 - n2)
+        else:
+            min_cost += min(c2, c3) * (n2 - n1)
+        return min_cost
+            
+class Solution1:
+    def minimumCost(self, c1: int, c2: int, c3: int, n1: int, n2: int) -> int:
         x = c1 + c2 - c3
         y = n1 * c1 + n2 * c2
         if x <= 0: 
