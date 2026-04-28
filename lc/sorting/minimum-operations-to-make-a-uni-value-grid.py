0856b21d72601e0ba0bf6c581067e19b2d4771fb
megafyk <megafyk@gmail.com>
2026-04-28 08:15:47 +0700

    Update minimum-operations-to-make-a-uni-value-grid.py

--- lc/sorting/minimum-operations-to-make-a-uni-value-grid.py
+++ lc/sorting/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,27 +1,39 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        # sorting + prefix sum
-        # time O(NlogN), space O(N)
-        arr = []
-        m = len(grid)
-        n = len(grid[0])
-        mod = grid[0][0] % x
+        m, n = len(grid), len(grid[0])
 
+        arr = []
+        mod = grid[0][0] % x
         for i in range(m):
             for j in range(n):
                 if grid[i][j] % x != mod:
                     return -1
                 arr.append(grid[i][j])
-        
-        arr = sorted(arr)
-        total = sum(arr)
+        arr.sort()
+        half = (m * n) // 2
+        if half == 0:
+            return 0
+        return (sum(arr[-half:]) - sum(arr[:half])) // x
 
-        res = sys.maxsize
+
+class Solution1:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        m, n = len(grid), len(grid[0])
+        total = 0
+        arr = []
+        mod = grid[0][0] % x
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] % x != mod:
+                    return -1
+                arr.append(grid[i][j])
+                total += grid[i][j]
+        arr.sort()
+        res = inf
         prefix = 0
-        for i in range(m*n):
-            left = arr[i] * i - prefix
-            right = total - prefix - (arr[i] * (n*m-i))
+        for i in range(m * n):
+            left = (arr[i] * i) - prefix
+            right = total - prefix - (arr[i] * (m * n - i))
             res = min(res, left + right)
             prefix += arr[i]
-            
         return res // x
