1f1fe3f8d07a04920e1b624448c527d146bcb705
megafyk <megafyk@gmail.com>
2026-05-06 08:34:40 +0700

    Update rotating-the-box.py

--- lc/two_pointers/rotating-the-box.py
+++ lc/two_pointers/rotating-the-box.py
@@ -1,48 +1,24 @@
 class Solution:
-    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        # matrix
+    def rotateTheBox(self, grid: List[List[str]]) -> List[List[str]]:
+        # 2 pointers
         # time O(m*n), space O(m*n)
-        m = len(box)
-        n = len(box[0])
-
-        for row in box:
-            i,j = 0,0
-            stones = 0
-            while j <= n:
-                if j == n or row[j] == "*":
-                    for t in range(j-1, i-1, -1):
-                        if stones > 0:
-                            row[t] = "#"
-                            stones -= 1
-                        else:
-                            row[t] = "."
-                    j+=1
-                    i=j
-                elif row[j] == "#":
-                    stones += 1
-                    j += 1
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            r = n - 1
+            l = r
+            while l >= 0:
+                if grid[i][l] == "*":
+                    l -= 1
+                    r = l
+                elif grid[i][l] == "#":
+                    grid[i][l], grid[i][r] = grid[i][r], grid[i][l]
+                    r -= 1
+                    l -= 1
                 else:
-                    j += 1
-        rotate = [["*"] * m for _ in range(n)]
+                    l -= 1
 
+        res = [["."] * m for _ in range(n)]
         for i in range(m):
             for j in range(n):
-                rotate[j][m-1-i] = box[i][j]
-        return rotate
-
-    # def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-    #     m = len(box)
-    #     n = len(box[0])
-    #     ans = [['.'] * m for _ in range(n)]
-    #     for i in range(m):
-    #         k = n - 1
-    #         for j in range(n - 1, -1, -1):
-    #             if box[i][j] == "*":
-    #                 k = j - 1
-    #                 ans[j][m - i - 1] = box[i][j]
-    #             elif box[i][j] == "#":
-    #                 box[i][j], box[i][k] = box[i][k], box[i][j]
-    #                 ans[j][m - i - 1], ans[k][m - i - 1] = box[i][j], box[i][k]
-    #                 k -= 1
-
-    #     return ans
+                res[j][m - 1 - i] = grid[i][j]
+        return res
