3984f6e52d68a429ed7ad50244a164c380420cf8
megafyk <megafyk@gmail.com>
2026-05-19 07:59:18 +0700

    Create minimum-common-value.py

---
+++ lc/two_pointers/minimum-common-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        # time O(min(m,n)), space O(1)
+        m, n = len(nums1), len(nums2)
+        i, j = 0, 0
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                return nums1[i]
+        return -1
