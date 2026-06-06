82cbff22b70306d531f9b08f06d7701a61bb4b38
megafyk <megafyk@gmail.com>
2026-06-06 22:13:31 +0700

    add dsa project

---
+++ dsa/tests/algorithms/.template.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+// #include "algorithms/your_header.h"
+
+// using namespace dsa;
+
+// TEST(YourAlgorithm, BasicCase) {
+//     // Arrange
+//
+//     // Act
+//
+//     // Assert
+//     EXPECT_EQ(1, 1);
+// }
---
+++ dsa/tests/data_structures/test_hash_table.cpp
@@ -0,0 +1,6 @@
+
+// tests/data_structures/test_hash_table.cpp
+#include "data_structures/hash_table.h"
+#include <gtest/gtest.h>
+
+TEST(HashTable, Placeholder) { EXPECT_TRUE(false); }
