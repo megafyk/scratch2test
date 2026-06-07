21792bb25aa7a861308d2e620abe6631657fcde1
megafyk <megafyk@gmail.com>
2026-06-08 05:43:33 +0700

    update hashtable

--- dsa/include/data_structures/hash_table.h
+++ dsa/include/data_structures/hash_table.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstddef>
+#include <optional>
+#include <string>
+#include <vector>
 namespace dsa {
 
 template <typename Key, typename Value>
@@ -9,12 +12,42 @@ private:
   struct Data {
     Key key;
     Value val;
+    std::string status;
   };
   std::size_t size_;
-  Data data_[];
+  std::vector<std::optional<Data>> bucket_;
 
 public:
-  void add(Key key, Value val);
+  HashTable(int size) : bucket_(size) { size_ = size; }
+  std::vector<std::optional<Data>> &bucket() { return bucket_; }
+  std::size_t size() { return size_; }
+  void add(Key key, Value val) {
+    size_t start = key % size_;
+    size_t cur = start;
+    size_t first_deleted_idx = -1;
+    while (true) {
+      std::optional<Data> cur_val = bucket_[cur];
+      if (!cur_val.has_value()) {
+        size_t target_idx = (first_deleted_idx == -1) ? cur : first_deleted_idx;
+        bucket_[target_idx] = {key, val, "ACTIVE"};
+        return;
+      } else if (bucket_[cur].value().status == "TOMBSTONE") {
+        if (first_deleted_idx == -1) {
+          first_deleted_idx = cur;
+        }
+      } else {
+        if (bucket_[cur].value().key == key) {
+          // update key
+          bucket_[cur] = {key, val, "ACTIVE"};
+        }
+      }
+      cur = (cur + 1) % size_;
+      if (cur == start and first_deleted_idx == -1) {
+        // hashtable full
+        return;
+      }
+    }
+  }
   bool exists(Key key);
   Value get(Key key);
   void remove(Key key);
--- dsa/tests/data_structures/test_hash_table.cpp
+++ dsa/tests/data_structures/test_hash_table.cpp
@@ -1,6 +1,20 @@
 
 // tests/data_structures/test_hash_table.cpp
 #include "data_structures/hash_table.h"
+#include <cstddef>
 #include <gtest/gtest.h>
 
-TEST(HashTable, Placeholder) { EXPECT_TRUE(false); }
+TEST(HashTable, constructor) {
+    int universal = 7;
+    dsa::HashTable<int, int> hashTable(universal);
+    EXPECT_EQ(universal, hashTable.bucket().size());
+    EXPECT_EQ(universal, hashTable.size());
+}
+
+TEST(HashTable, add) {
+    int universal = 7;
+    dsa::HashTable<int, std::string> hashTable(universal);
+    hashTable.add(1, "red");
+    EXPECT_EQ(1, hashTable.bucket()[1].value().key);
+    EXPECT_EQ("red", hashTable.bucket()[1].value().val);
+}
