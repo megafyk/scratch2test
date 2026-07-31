75d00c65940b19100e845194d919d68bf6c244e2
megafyk <megafyk@gmail.com>
2026-07-31 08:14:49 +0700

    Update minimum-number-of-pushes-to-type-word-i.py

--- lc/sorting/minimum-number-of-pushes-to-type-word-i.py
+++ lc/sorting/minimum-number-of-pushes-to-type-word-i.py
@@ -1,5 +1,14 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        x = n // 8
+        if x >= 3: return 8 + 16 + 24 + 4 * (n-24)
+        elif x >= 2: return 8 + 16 + 3 * (n-16)
+        elif x >= 1: return 8 + 2 * (n-8)
+        return n
+
+class Solution1:
+    def minimumPushes(self, word: str) -> int:
         # sorting
         # time O(n), space O(1)
         cnt = [0] * 26
